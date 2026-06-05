3437b62b8ca932517fe933d78e7b8fd9d6c8f8ec
ajle2721 <ajle2721@gmail.com>
2026-06-05 14:24:10 +0800

    Data: Align local index compiler with node builder output by adding price_level and cuisine properties

--- fix_index_now.ps1
+++ fix_index_now.ps1
@@ -59,6 +59,41 @@ foreach ($file in $aiFiles) {
             foreach ($d in $taipeiDistricts) { if ($address.Contains($d)) { $district = $d; break } }
             if ($address.Contains("$([char]0x4e2d)$([char]0x6b63)")) { $district = "$([char]0x4e2d)$([char]0x6b63)$([char]0x5340)" }
             
+            $priceLevel = if ($response.priceLevel) { $response.priceLevel } else { $null }
+            $cuisineMap = @{
+                'italian_restaurant' = "$([char]0x7fa9)$([char]0x5927)$([char]0x5229)$([char]0x6599)$([char]0x7406)"
+                'japanese_restaurant' = "$([char]0x65e5)$([char]0x5f0f)$([char]0x6599)$([char]0x7406)"
+                'korean_restaurant' = "$([char]0x97d3)$([char]0x5f0f)$([char]0x6599)$([char]0x7406)"
+                'chinese_restaurant' = "$([char]0x4e2d)$([char]0x5f0f)$([char]0x6599)$([char]0x7406)"
+                'thai_restaurant' = "$([char]0x6cf0)$([char]0x5f0f)$([char]0x6599)$([char]0x7406)"
+                'french_restaurant' = "$([char]0x6cd5)$([char]0x5f0f)$([char]0x6599)$([char]0x7406)"
+                'american_restaurant' = "$([char]0x7f8e)$([char]0x5f0f)$([char]0x6599)$([char]0x7406)"
+                'mexican_restaurant' = "$([char]0x58a8)$([char]0x897f)$([char]0x54e5)$([char]0x6599)$([char]0x7406)"
+                'vietnamese_restaurant' = "$([char]0x8d8a)$([char]0x5357)$([char]0x6599)$([char]0x7406)"
+                'vegetarian_restaurant' = "$([char]0x852c)$([char]0x98df)$([char]0x6599)$([char]0x7406)"
+                'steak_house' = "$([char]0x725b)$([char]0x6392)$([char]0x9928)"
+                'sushi_restaurant' = "$([char]0x583d)$([char]0x53f8)"
+                'pizza_restaurant' = "$([char]0x62ab)$([char]0x85a9)"
+                'ramen_restaurant' = "$([char]0x62c9)$([char]0x9eb5)"
+                'cafe' = "$([char]0x5496)$([char]0x5561)$([char]0x5ec3)"
+                'bakery' = "$([char]0x70d8)$([char]0x57f9)/$([char]0x751c)$([char]0x9ede)"
+                'bar' = "$([char]0x9152)$([char]0x5427)/$([char]0x9910)$([char]0x9152)$([char]0x9928)"
+                'bistro' = "$([char]0x5c0f)$([char]0x9152)$([char]0x9928)/$([char]0x9910)$([char]0x9152)$([char]0x9928)"
+                'brunch_restaurant' = "$([char]0x65e9)$([char]0x5348)$([char]0x9910)"
+            }
+            $cuisine = $null
+            if ($response.types -is [array]) {
+                foreach ($t in $response.types) {
+                    if ($cuisineMap.ContainsKey($t)) {
+                        $cuisineLabel = $cuisineMap[$t]
+                        if (-not $name.Contains($cuisineLabel)) {
+                            $cuisine = $cuisineLabel
+                            break
+                        }
+                    }
+                }
+            }
+
             $googleMapsUrl = "https://www.google.com/maps/search/?api=1&query=$([Uri]::EscapeDataString($name))&query_place_id=$placeId"
             $signals = if ($aiReview.generated_signals -is [array]) { $aiReview.generated_signals } else { @($aiReview.generated_signals) }
             
@@ -74,6 +109,8 @@ foreach ($file in $aiFiles) {
                 "district" = $district
                 "rating" = if ($response.rating) { $response.rating.ToString() } else { "" }
                 "user_ratings_total" = if ($response.userRatingCount) { $response.userRatingCount } else { 0 }
+                "price_level" = $priceLevel
+                "cuisine" = $cuisine
                 "latitude" = if ($response.location) { $response.location.latitude } else { $null }
                 "longitude" = if ($response.location) { $response.location.longitude } else { $null }
                 "url" = $googleMapsUrl
