265bddd5e83f057142e37c973bd85ae1c39a0d6e
ajle2721 <ajle2721@gmail.com>
2026-06-10 16:21:18 +0800

    update

--- update_index.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/app\.js\?v=\d+/, 'app.js?v=20260610160800');
-html = html.replace(/ai_review\/index\.js\?v=\d+/, 'ai_review/index.js?v=20260610160800');
-fs.writeFileSync('index.html', html);
-console.log('updated index.html');
